029ec23f1bdd3d80cbd7b6d54a9a987bb5349b54
vritanttiple <vritanttiple017@gmail.com>
2022-02-24 15:57:50 +0530

    commit

---
+++ demo_git_project/src/demo_git_project/Test1.java
@@ -0,0 +1,7 @@
+package demo_git_project;
+
+public class Test1 {
+public static void main(String[] args) {
+	System.out.println("Welcome to git");
+}
+}
---
+++ demo_git_project/src/demo_git_project/git1.java
@@ -0,0 +1,8 @@
+package demo_git_project;
+
+public class git1 {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome again!");
+}
+}
